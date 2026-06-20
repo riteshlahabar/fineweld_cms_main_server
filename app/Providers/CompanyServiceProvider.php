e23336c6441caa8530167baad3c494a18f78c2bf
riteshlahabar <riteshlahabar@gmail.com>
2026-06-20 18:16:21 +0530

    20-06-26

--- app/Providers/CompanyServiceProvider.php
+++ app/Providers/CompanyServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use App\Enums\App;
 use App\Enums\Date;
 use App\Enums\Timezone;
-use App\Models\Company;
 use App\Services\CacheService;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Config;
@@ -21,9 +20,7 @@ class CompanyServiceProvider extends ServiceProvider
         if (env('INSTALLATION_STATUS')) {
             // Bind the timezone to a service
             $this->app->singleton('company', function () {
-
-                // Model
-                $company = CacheService::get('company'); // Company::find(App::APP_SETTINGS_RECORD_ID->value);
+                $company = CacheService::get('company');
 
                 $timezone = $company ? $company->timezone : Timezone::APP_DEFAULT_TIME_ZONE->value;
 
@@ -45,48 +42,48 @@ class CompanyServiceProvider extends ServiceProvider
                     'date_format' => $dateFormat,
                     'time_format' => $timeFormat,
                     'active_sms_api' => $active_sms_api,
-                    'number_precision' => $company->number_precision,
-                    'quantity_precision' => $company->quantity_precision,
+                    'number_precision' => $company->number_precision ?? 2,
+                    'quantity_precision' => $company->quantity_precision ?? 2,
 
-                    'show_sku' => $company->show_sku, // Item Settings, Sidebar-> Settings -> Company ->Item
-                    'show_mrp' => $company->show_mrp, // Item Settings, Sidebar-> Settings -> Company ->Item
-                    'restrict_to_sell_above_mrp' => $company->restrict_to_sell_above_mrp, // Item Settings, Sidebar-> Settings -> Company ->Item
-                    'restrict_to_sell_below_msp' => $company->restrict_to_sell_below_msp, // Item Settings, Sidebar-> Settings -> Company ->Item
-                    'auto_update_sale_price' => $company->auto_update_sale_price, // Item Settings, Sidebar-> Settings -> Company ->Item
-                    'auto_update_purchase_price' => $company->auto_update_purchase_price, // Item Settings, Sidebar-> Settings -> Company ->Item
-                    'auto_update_average_purchase_price' => $company->auto_update_average_purchase_price, // Item Settings, Sidebar-> Settings -> Company ->Item
+                    'show_sku' => $company->show_sku ?? 1, // Item Settings, Sidebar-> Settings -> Company ->Item
+                    'show_mrp' => $company->show_mrp ?? 1, // Item Settings, Sidebar-> Settings -> Company ->Item
+                    'restrict_to_sell_above_mrp' => $company->restrict_to_sell_above_mrp ?? 0, // Item Settings, Sidebar-> Settings -> Company ->Item
+                    'restrict_to_sell_below_msp' => $company->restrict_to_sell_below_msp ?? 0, // Item Settings, Sidebar-> Settings -> Company ->Item
+                    'auto_update_sale_price' => $company->auto_update_sale_price ?? 0, // Item Settings, Sidebar-> Settings -> Company ->Item
+                    'auto_update_purchase_price' => $company->auto_update_purchase_price ?? 0, // Item Settings, Sidebar-> Settings -> Company ->Item
+                    'auto_update_average_purchase_price' => $company->auto_update_average_purchase_price ?? 0, // Item Settings, Sidebar-> Settings -> Company ->Item
 
-                    'is_item_name_unique' => $company->is_item_name_unique, // Item Settings, Sidebar-> Settings -> Company ->Item
-                    'tax_type' => $company->tax_type, // Item Settings, Sidebar-> Settings -> Company ->Item
+                    'is_item_name_unique' => $company->is_item_name_unique ?? 1, // Item Settings, Sidebar-> Settings -> Company ->Item
+                    'tax_type' => $company->tax_type ?? 'tax', // Item Settings, Sidebar-> Settings -> Company ->Item
 
-                    'enable_serial_tracking' => $company->enable_serial_tracking, // Item Settings, Sidebar-> Settings -> Company ->Item
-                    'enable_batch_tracking' => $company->enable_batch_tracking, // Item Settings, Sidebar-> Settings -> Company ->Item
-                    'is_batch_compulsory' => $company->is_batch_compulsory, // Item Settings, Sidebar-> Settings -> Company ->Item
-                    'enable_mfg_date' => $company->enable_mfg_date, // Item Settings, Sidebar-> Settings -> Company ->Item
-                    'enable_exp_date' => $company->enable_exp_date, // Item Settings, Sidebar-> Settings -> Company ->Item
-                    'enable_color' => $company->enable_color, // Item Settings, Sidebar-> Settings -> Company ->Item
-                    'enable_size' => $company->enable_size, // Item Settings, Sidebar-> Settings -> Company ->Item
-                    'enable_model' => $company->enable_model, // Item Settings, Sidebar-> Settings -> Company ->Item
+                    'enable_serial_tracking' => $company->enable_serial_tracking ?? 1, // Item Settings, Sidebar-> Settings -> Company ->Item
+                    'enable_batch_tracking' => $company->enable_batch_tracking ?? 2, // Item Settings, Sidebar-> Settings -> Company ->Item
+                    'is_batch_compulsory' => $company->is_batch_compulsory ?? 0, // Item Settings, Sidebar-> Settings -> Company ->Item
+                    'enable_mfg_date' => $company->enable_mfg_date ?? 1, // Item Settings, Sidebar-> Settings -> Company ->Item
+                    'enable_exp_date' => $company->enable_exp_date ?? 1, // Item Settings, Sidebar-> Settings -> Company ->Item
+                    'enable_color' => $company->enable_color ?? 0, // Item Settings, Sidebar-> Settings -> Company ->Item
+                    'enable_size' => $company->enable_size ?? 0, // Item Settings, Sidebar-> Settings -> Company ->Item
+                    'enable_model' => $company->enable_model ?? 0, // Item Settings, Sidebar-> Settings -> Company ->Item
 
-                    'show_tax_summary' => $company->show_tax_summary, // Print Settings, Sidebar-> Settings -> Company ->Print
-                    'state_id' => $company->state_id, // Print Settings, Sidebar-> Settings -> Company ->Print
-                    'terms_and_conditions' => $company->terms_and_conditions, // Print Settings, Sidebar-> Settings -> Company ->Print
-                    'show_terms_and_conditions_on_invoice' => $company->show_terms_and_conditions_on_invoice, // Print Settings, Sidebar-> Settings -> Company ->Print
-                    'show_party_due_payment' => $company->show_party_due_payment, // Print Settings, Sidebar-> Settings -> Company ->Print
-                    'bank_details' => $company->bank_details, // Print Settings, Sidebar-> Settings -> Company ->Print
-                    'signature' => $company->signature, // Print Settings, Sidebar-> Settings -> Company ->Print
-                    'show_signature_on_invoice' => $company->show_signature_on_invoice, // Print Settings, Sidebar-> Settings -> Company ->Print
-                    'show_brand_on_invoice' => $company->show_brand_on_invoice, // Item Settings, Sidebar-> Settings -> Company ->Print
-                    'show_tax_number_on_invoice' => $company->show_tax_number_on_invoice, // Item Settings, Sidebar-> Settings -> Company ->Print
-                    'colored_logo' => $company->colored_logo, // Print Settings, Sidebar-> Settings -> Company ->Print
+                    'show_tax_summary' => $company->show_tax_summary ?? 1, // Print Settings, Sidebar-> Settings -> Company ->Print
+                    'state_id' => $company->state_id ?? null, // Print Settings, Sidebar-> Settings -> Company ->Print
+                    'terms_and_conditions' => $company->terms_and_conditions ?? null, // Print Settings, Sidebar-> Settings -> Company ->Print
+                    'show_terms_and_conditions_on_invoice' => $company->show_terms_and_conditions_on_invoice ?? 1, // Print Settings, Sidebar-> Settings -> Company ->Print
+                    'show_party_due_payment' => $company->show_party_due_payment ?? 1, // Print Settings, Sidebar-> Settings -> Company ->Print
+                    'bank_details' => $company->bank_details ?? null, // Print Settings, Sidebar-> Settings -> Company ->Print
+                    'signature' => $company->signature ?? null, // Print Settings, Sidebar-> Settings -> Company ->Print
+                    'show_signature_on_invoice' => $company->show_signature_on_invoice ?? 1, // Print Settings, Sidebar-> Settings -> Company ->Print
+                    'show_brand_on_invoice' => $company->show_brand_on_invoice ?? 1, // Item Settings, Sidebar-> Settings -> Company ->Print
+                    'show_tax_number_on_invoice' => $company->show_tax_number_on_invoice ?? 1, // Item Settings, Sidebar-> Settings -> Company ->Print
+                    'colored_logo' => $company->colored_logo ?? null, // Print Settings, Sidebar-> Settings -> Company ->Print
 
-                    'is_enable_crm' => $isEnableCrm, // Print Settings, Sidebar-> Settings -> Company ->Module
-                    'is_enable_carrier' => $company->is_enable_carrier, // Print Settings, Sidebar-> Settings -> Company ->Module
-                    'is_enable_carrier_charge' => $company->is_enable_carrier_charge, // Print Settings, Sidebar-> Settings -> Company ->General
-                    'show_discount' => $company->show_discount, // Enable Discount Setting: Sidebar-> Settings -> Company ->General
-                    'allow_negative_stock_billing' => $company->allow_negative_stock_billing, // Enable Negative Stock Billing - Setting: Sidebar-> Settings -> Company ->General
-                    'show_hsn' => $company->show_hsn, // Item Settings, Sidebar-> Settings -> Company ->Item
-                    'is_enable_secondary_currency' => $company->is_enable_secondary_currency, // Item Settings, Sidebar-> Settings -> Company ->General
+                    'is_enable_crm' => $isEnableCrm ?? 0, // Print Settings, Sidebar-> Settings -> Company ->Module
+                    'is_enable_carrier' => $company->is_enable_carrier ?? 1, // Print Settings, Sidebar-> Settings -> Company ->Module
+                    'is_enable_carrier_charge' => $company->is_enable_carrier_charge ?? 1, // Print Settings, Sidebar-> Settings -> Company ->General
+                    'show_discount' => $company->show_discount ?? 1, // Enable Discount Setting: Sidebar-> Settings -> Company ->General
+                    'allow_negative_stock_billing' => $company->allow_negative_stock_billing ?? 1, // Enable Negative Stock Billing - Setting: Sidebar-> Settings -> Company ->General
+                    'show_hsn' => $company->show_hsn ?? 1, // Item Settings, Sidebar-> Settings -> Company ->Item
+                    'is_enable_secondary_currency' => $company->is_enable_secondary_currency ?? 1, // Item Settings, Sidebar-> Settings -> Company ->General
 
                 ];
             });
@@ -99,8 +96,10 @@ class CompanyServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (env('INSTALLATION_STATUS')) {
+            $company = app('company');
+
             // Set the default timezone
-            date_default_timezone_set(app('company')['timezone']);
+            date_default_timezone_set($company['timezone']);
 
             // Use the timezone and date format in Carbon
             // Carbon::setLocale(app('company')['timezone']);
@@ -115,8 +114,13 @@ class CompanyServiceProvider extends ServiceProvider
             /**
              * Email setup
              * */
-            Config::set('mail.from.address', app('company')['email']);
-            Config::set('mail.from.name', app('company')['name']);
+            if (! empty($company['email'])) {
+                Config::set('mail.from.address', $company['email']);
+            }
+
+            if (! empty($company['name'])) {
+                Config::set('mail.from.name', $company['name']);
+            }
         }
     }
 }
--- app/Services/CacheService.php
+++ app/Services/CacheService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Schema;
 
 class CacheService
 {
@@ -11,38 +12,53 @@ class CacheService
         if (env('INSTALLATION_STATUS')) {
             switch ($type) {
                 case 'tax':
-                    return Cache::remember('tax', now()->addHours(24), function () {
-                        return \App\Models\Tax::all();
-                    });
+                    return self::rememberAll('tax', \App\Models\Tax::class);
                 case 'unit':
-                    return Cache::remember('unit', now()->addHours(24), function () {
-                        return \App\Models\Unit::all();
-                    });
+                    return self::rememberAll('unit', \App\Models\Unit::class);
                 case 'appSetting':
-                    return Cache::remember('appSetting', now()->addHours(24), function () {
-                        return \App\Models\AppSettings::first();
-                    });
+                    return self::rememberFirst('appSetting', \App\Models\AppSettings::class);
                 case 'company':
-                    return Cache::remember('company', now()->addHours(24), function () {
-                        return \App\Models\Company::first();
-                    });
+                    return self::rememberFirst('company', \App\Models\Company::class);
                 case 'warehouse':
-                    return Cache::remember('warehouse', now()->addHours(24), function () {
-                        return \App\Models\Warehouse::all();
-                    });
+                    return self::rememberAll('warehouse', \App\Models\Warehouse::class);
                 case 'smtpSettings':
-                    return Cache::remember('smtpSettings', now()->addHours(24), function () {
-                        return \App\Models\SmtpSettings::first() ?? (object) [
-                            'host' => null,
-                            'port' => null,
-                            'username' => null,
-                            'password' => null,
-                            'encryption' => null,
-                        ];
-                    });
+                    return self::rememberFirst('smtpSettings', \App\Models\SmtpSettings::class, (object) [
+                        'host' => null,
+                        'port' => null,
+                        'username' => null,
+                        'password' => null,
+                        'encryption' => null,
+                    ]);
                 default:
                     throw new \Exception("Invalid cache type: $type");
             }
         }
+    }
+
+    protected static function rememberAll(string $cacheKey, string $modelClass)
+    {
+        if (! self::hasTable($modelClass)) {
+            return collect();
+        }
+
+        return Cache::remember($cacheKey, now()->addHours(24), function () use ($modelClass) {
+            return $modelClass::all();
+        });
+    }
+
+    protected static function rememberFirst(string $cacheKey, string $modelClass, mixed $default = null)
+    {
+        if (! self::hasTable($modelClass)) {
+            return $default;
+        }
+
+        return Cache::remember($cacheKey, now()->addHours(24), function () use ($modelClass, $default) {
+            return $modelClass::first() ?? $default;
+        });
+    }
+
+    protected static function hasTable(string $modelClass): bool
+    {
+        return Schema::hasTable((new $modelClass)->getTable());
     }
 }
